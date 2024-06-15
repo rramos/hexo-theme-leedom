f88151fd04ff24cb4668bd569b920a82289a20c4
Rui Ramos <rui.ms.ramos@gmail.com>
2024-06-15 02:10:06 +0100

    Added newsletter componnet (draft) and testing webpusher

--- source/js/klaro-config.js
+++ source/js/klaro-config.js
@@ -31,6 +31,7 @@ var klaroConfig = {
         {
             // In GTM, you should define a custom event trigger named `klaro-google-analytics-accepted` which should trigger the Google Analytics integration.
             name: 'google-analytics',
+            required: true,
             cookies: [
                 /^_ga(_.*)?/ // we delete the Google Analytics cookies if the user declines its use
             ],
@@ -52,7 +53,34 @@ var klaroConfig = {
                     description: 'Google Analytics is a free tool for tracking and analyzing website traffic and user behavior'
                 },
             },
-        }
+        },
+        {
+            name: 'webpusher',
+            required: false,
+            purposes: ['marketing'],
+            onAccept: `
+                // we notify the tag manager about all services that were accepted. You can define
+                // a custom event in GTM to load the service if consent was given.
+                for(let k of Object.keys(opts.consents)){
+                    if (opts.consents[k]){
+                        let eventName = 'klaro-'+k+'-accepted'
+                        dataLayer.push({'event': eventName})
+                    }
+                }
+            `,
+            onInit: `
+                // initialization code here (will be executed only once per page-load)
+                window.dataLayer = window.dataLayer || [];
+                window.gtag = function(){dataLayer.push(arguments)}
+                gtag('consent', 'default', {'ad_storage': 'denied', 'analytics_storage': 'denied', 'ad_user_data': 'denied', 'ad_personalization': 'denied'})
+                gtag('set', 'ads_data_redaction', true)
+            `,
+            translations: {
+                en: {
+                    description: 'Webpushr is a web push notifications platform that marketers love & developers rely on'
+                },
+            },
+        },
     ],
     purposes: {
                 analytics: {
